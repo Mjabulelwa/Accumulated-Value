ab89de74e5f2a9a88d4f7d508cebbe156a10ecdb
Mjabulelwa <MjabulelwaWelford@gmail.com>
2025-02-24 02:55:52 +0200

    first commit

---
+++ AccumulatedValue/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿
+using AccumulatedValue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccumulatedValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mjabulelwa";
+            ViewBag.AV = 99999.99;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AccumulatedValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.AV = model.CalculateAccumulatedValue();
+            }
+            else
+            {
+                ViewBag.AV = 0;
+            }
+            return View(model);
+        }
+    }
+}
